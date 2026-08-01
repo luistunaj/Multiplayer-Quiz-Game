ee9f559b0cd04300555e85d1b1e0ac2e21ba0a7e
luistunaj <242400715+luistunaj@users.noreply.github.com>
2026-08-01 04:09:13 +0200

    Accepts a TCP connection and log incoming lines.

--- src/main/java/quiz/server/QuizServer.java
+++ src/main/java/quiz/server/QuizServer.java
@@ -1,9 +1,15 @@
 package quiz.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 public class QuizServer {
@@ -22,8 +28,27 @@ public class QuizServer {
         }
     }
 
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) throws IOException {
+        int port = 5001;
+
         System.out.println("Quiz Server");
-        printLanAddresses(5000);
+        printLanAddresses(port);
+
+        try (ServerSocket server = new ServerSocket(port)) {
+            System.out.println("Listening on port: " + port);
+
+            try (Socket socket = server.accept(); BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))) {
+
+                System.out.println("Connected: " + socket.getRemoteSocketAddress());
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.out.println("[in] " + line);
+                }
+
+                System.out.println("Disconnected: " + socket.getRemoteSocketAddress());
+            }
+        }
     }
 }
