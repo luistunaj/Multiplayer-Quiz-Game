2fc5799304b0875baa2515c296cd3922572b7a7b
luistunaj <242400715+luistunaj@users.noreply.github.com>
2026-08-01 03:53:03 +0200

    Prints LAN addresses on server startup based on the port written.

--- src/main/java/quiz/server/QuizServer.java
+++ src/main/java/quiz/server/QuizServer.java
@@ -1,8 +1,29 @@
 package quiz.server;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+
 public class QuizServer {
 
-    public static void main(String[] args) {
+    private static void printLanAddresses(int port) throws SocketException {
+        for (NetworkInterface nif : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+            if (!nif.isUp() || nif.isLoopback()) {
+                continue;
+            }
+            for (InetAddress addr : Collections.list(nif.getInetAddresses())) {
+                if (!(addr instanceof Inet4Address)) {
+                    continue;
+                }
+                System.out.println("Join with: " + addr.getHostAddress() + " port " + port);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SocketException {
         System.out.println("Quiz Server");
+        printLanAddresses(5000);
     }
 }
